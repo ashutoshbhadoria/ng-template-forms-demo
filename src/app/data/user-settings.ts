f790998e895341e44c0dc4fdb1902fb81b645116
Bhadoria, Ashutosh <ashutoshbhadoria@outlook.com>
2021-11-17 02:36:44 +0530

    Move template to the correct location. Add 2-way data binding.

---
+++ src/app/data/user-settings.ts
@@ -0,0 +1,7 @@
+export interface UserSettings {
+  name: string;
+  emailOffers: boolean;
+  userInterfaceStyle: string;
+  subscriptionType: string;
+  notes: string;
+}
--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
-  styleUrls: ['./user-settings-form.component.css']
+  styleUrls: ['./user-settings-form.component.css'],
 })
 export class UserSettingsFormComponent implements OnInit {
+  originalUserSettings: UserSettings = {
+    name: 'Ryouta Kise',
+    emailOffers: true,
+    userInterfaceStyle: 'dark',
+    subscriptionType: 'Lifetime',
+    notes: 'Aominechi I will defeat you.',
+  };
 
-  constructor() { }
+  userSettings: UserSettings = { ...this.originalUserSettings };
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
